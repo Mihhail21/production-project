c1f8b8d52efde2c93693425f4559f9a022ee4ab1
Mikhail21 <saveshit8@gmail.com>
2022-09-20 22:36:36 +0300

    sidebar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,6 +3,7 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { useTheme } from './providers/ThemeProvider';
 import { AppRouter } from './providers/router';
 import { NavBar } from 'widgets';
+import { Sidebar } from 'widgets/Sidebar';
 
 
 function App() {
@@ -12,7 +13,10 @@ function App() {
   return (
     <div className={classNames('app', {}, [theme])}>
           <NavBar/>
-         <AppRouter/>
+          <div className='content-page'> 
+            <Sidebar/>
+            <AppRouter/>
+          </div>
     </div>
   )
 }
--- src/widgets/NavBar/ui/NavBar.tsx
+++ src/widgets/NavBar/ui/NavBar.tsx
@@ -11,7 +11,7 @@ interface NavbarProps {
 export const NavBar = ({className}: NavbarProps) => {
   return (
      <div className={classNames(cls.Navbar, {}, [className])}>
-       <ThemeSwitcher/>
+       
             <div className={cls.links}>
               <AppLink theme={AppLinkTheme.SECONDARY} to={'/'} className={cls.mainLink}> Главная</AppLink>
               <AppLink theme={AppLinkTheme.RED} to={'/about'}> О сайте</AppLink>
