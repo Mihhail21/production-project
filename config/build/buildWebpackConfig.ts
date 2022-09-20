f7df88f8e54ed4c2b595435b1fb9317bd4da372d
Mikhail21 <saveshit8@gmail.com>
2022-09-20 11:59:47 +0300

    architecture task

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -21,7 +21,7 @@ return {
     // лоудеры самое главное 
     rules: buildLoaders(options)
   },
-  resolve: buildResolvers(),
+  resolve: buildResolvers(options),
   devtool: isDev ? 'inline-source-map' : undefined,
   devServer: isDev ? buildDevServer(options) : undefined,
 }
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,15 @@
 import { Suspense, useContext, useState } from 'react';
 import { Route, Routes, Link } from 'react-router-dom';
-import { Counter } from './components/Counter';
 import './styles/index.scss';
-import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
-import { MainPageAsync } from './pages/MainPage/MainPage.async';
-import { ThemeContext } from './theme/ThemeContext';
-import { useTheme } from './theme/useTheme';
-import { classNames } from './helpers/classNames/classNames';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTheme } from './providers/ThemeProvider';
+import { AboutPage } from 'pages/AboutPage';
+import { MainPage } from 'pages/MainPage';
 
 
 function App() {
   const {theme, toggleTheme} = useTheme()
-  const bool = true; 
+ 
  
   return (
     <div className={classNames('app', {}, [theme])}>
@@ -20,11 +18,10 @@ function App() {
           <Link to={'/about'}> О сайте</Link>
           <Suspense fallback={<div>Loading...</div>}>
               <Routes>
-                  <Route path={'/about'} element={<AboutPageAsync/>}/>
-                  <Route path={'/'} element={<MainPageAsync/>}/>
+                  <Route path={'/about'} element={<AboutPage/>}/>
+                  <Route path={'/'} element={<MainPage/>}/>
               </Routes>
         </Suspense>
-          <Counter/>
     </div>
   )
 }
