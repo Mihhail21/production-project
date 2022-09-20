52dc9ff7130806cf0d871dfc2a5359ba17afe3c6
Mikhail21 <saveshit8@gmail.com>
2022-09-20 13:20:51 +0300

    appRouter config

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,6 +5,7 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { useTheme } from './providers/ThemeProvider';
 import { AboutPage } from 'pages/AboutPage';
 import { MainPage } from 'pages/MainPage';
+import { AppRouter } from './providers/router';
 
 
 function App() {
@@ -16,12 +17,7 @@ function App() {
           <button onClick={toggleTheme}>toggle</button>
           <Link to={'/'}> Главная</Link>
           <Link to={'/about'}> О сайте</Link>
-          <Suspense fallback={<div>Loading...</div>}>
-              <Routes>
-                  <Route path={'/about'} element={<AboutPage/>}/>
-                  <Route path={'/'} element={<MainPage/>}/>
-              </Routes>
-        </Suspense>
+         <AppRouter/>
     </div>
   )
 }
