b45e8fc41743c50d409268a899fc67167c9e63fa
Mikhail21 <saveshit8@gmail.com>
2022-09-20 17:02:54 +0300

    ui kit navbar task

--- src/shared/ui/AppLink/AppLink.tsx
+++ src/shared/ui/AppLink/AppLink.tsx
@@ -26,7 +26,7 @@ export const AppLink: FC<AppLinkProps> = (props) => {
     return (
         <Link
             to={to}
-            className={classNames(cls.AppLink, {[cls[theme]]: true}, [className])}
+            className={classNames(cls.AppLink, {}, [className, cls[theme]])}
             {...otherProps}
         >
             {children}
@@ -34,3 +34,4 @@ export const AppLink: FC<AppLinkProps> = (props) => {
     );
 };
 
+// className={classNames(cls.AppLink, {}, [className, cls[theme]])}
